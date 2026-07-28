c58215debfe8606fbc3158381cbe0b808b842e3c
2005deepalakshmi <2005deepam@gmail.com>
2026-07-28 19:38:04 +0530

    Implemented Web Search Agent with Gemini integration

---
+++ test.py
@@ -0,0 +1,11 @@
+from agents.web_search_agent import web_search_agent
+
+idea = input(Enter Startup Idea )
+
+report = web_search_agent(idea)
+
+print(n)
+print(=  80)
+print(AI STARTUP VALIDATION REPORT)
+print(=  80)
+print(report)
