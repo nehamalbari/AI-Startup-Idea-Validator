5d2eadf19916e7a8aefd4527b38647928fdab8cb
Neha <nmalbari26@gmail.com>
2026-08-04 17:31:01 +0530

    temp changes before switching

--- app/config.py
+++ app/config.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import os
+﻿import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
@@ -12,21 +11,4 @@ llm = ChatGoogleGenerativeAI(
     temperature=0,
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
-=======
-﻿import os
-from dotenv import load_dotenv
-from langchain_groq import ChatGroq
 
-load_dotenv()
-
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
-if not GROQ_API_KEY:
-    raise ValueError("GROQ_API_KEY is not set. Please add it to your .env file.")
-
-llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
-    api_key=GROQ_API_KEY,
-    temperature=0.3,
-)
->>>>>>> c9438dda1f6d913e1f24d6f177c756f519642e8c
